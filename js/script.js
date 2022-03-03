a0fb1902bd9c35d5e4c0e6db8322fd43eaa88e96
Nomi Vos <nvoswork@gmail.com>
2022-03-03 05:35:43 -0600

    fetches the languages the repos are coded in

--- js/script.js
+++ js/script.js
@@ -52,6 +52,20 @@ const displayRepos = function (repos) {
 repoList.addEventListener("click", function (e) {
   if (e.target.matches("h3")) {
     const repoName = e.target.innerText;
-    console.log(repoName);
+    getRepoInfo(repoName);
   }
-})
+});
+
+const getRepoInfo = async function (repoName) {
+  const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo = await fetchInfo.json();
+  console.log(repoInfo);
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
+
+  const languages = [];
+  for (const language in languageData) {
+    languages.push(language);
+  }
+  console.log(languages);
+};
